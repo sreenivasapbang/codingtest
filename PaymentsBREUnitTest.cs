b315b221ae8b40333467ce8647238fbc9f479a5e
sreenivasapbang <66455934+sreenivasapbang@users.noreply.github.com>
2020-06-29 09:11:26 +0530

    Update PaymentsBREUnitTest.cs
    Adding pass scenarios for all the payment action types

--- PaymentsBREUnitTest.cs
+++ PaymentsBREUnitTest.cs
@@ -4,11 +4,29 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace PaymentsBREV1.Tests
 {
+  [TestClass()]
   public class PaymentsBREUnitTest
   {
+    [TestMethod()]
     public void AllPaymentsTest()
     {
-    
+            AllPayments paymentBRE = new AllPayments(new GenericPaymentBRE());
+            //All positive scenarios for testing
+            Assert.AreEqual(true, paymentBRE.RunPaymentBRE("PyProduct"));
+            Assert.AreEqual(true, paymentBRE.RunPaymentBRE("Book"));
+
+            Assert.AreEqual(true, paymentBRE.RunPaymentBRE("Book"));
+            Assert.AreEqual(true, paymentBRE.RunPaymentBRE("Membership"));
+            Assert.AreEqual(true, paymentBRE.RunPaymentBRE("UpgradeMembership"));
+            Assert.AreEqual(true, paymentBRE.RunPaymentBRE("MemupOrUpgr"));
+            Assert.AreEqual(true, paymentBRE.RunPaymentBRE("Video"));
+            Assert.AreEqual(true, paymentBRE.RunPaymentBRE("PyProductOrBook"));
+
+
+            //To demonstrate the fail case
+            Assert.AreEqual(true, paymentBRE.RunPaymentBRE("Others"));
+            Assert.AreEqual(true, paymentBRE.RunPaymentBRE("OthersV2"));
+
     }
   }
 }
