0f011c177414e59ba7d725494360b68c2264d4f0
sreenivasapbang <66455934+sreenivasapbang@users.noreply.github.com>
2020-06-29 08:32:35 +0530

    Create PaymentsBREUnitTest.cs
    Committing New Unit Test file for testing the PaymentsBRE Class file which holds the logic for the  Business Rules Engine

---
+++ PaymentsBREUnitTest.cs
@@ -0,0 +1,14 @@
+using PaymentsBREV1;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PaymentsBREV1.Tests
+{
+  public class PaymentsBREUnitTest
+  {
+    public void AllPaymentsTest()
+    {
+    
+    }
+  }
+}
